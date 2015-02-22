1ac3b787931ecf073f8e6f74e16a669405d26275
Axji <axel.zenklusen@gmail.com>
2015-02-22 08:15:54 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,55 +8,60 @@ packages <- c("data.table", "reshape2")
 sapply(packages, require, character.only = TRUE, quietly = TRUE)
 
 
+##Load All Files
+
+dt_Subject_Train <- read.table("train\\subject_train.txt",header=FALSE)
+dt_Subject_Test <- read.table("test\\subject_test.txt",header=FALSE)
+
+dt_Y_Train <- read.table("train\\Y_train.txt",header=FALSE)
+dt_Y_Test <- read.table("test\\Y_test.txt",header=FALSE)
+
+dt_X_Train <- read.table("train\\X_train.txt",header=FALSE)
+dt_X_Test <- read.table("test\\X_test.txt",header=FALSE)
+
+
+dt_Features <- read.table("features.txt",header=FALSE)
+dt_Activity_labels <- read.table("activity_labels.txt",header=FALSE);
+
+
+
+## Name COlumns
+colnames(dtActivity_labels)  = c('activityId','activityType');
+colnames(dtTrain)        = dtFeatures[,2]; 
+colnames(yTrain)        = "activityId";
+colnames(subjectTrain)  = "subjectId";
+
 
 ##Merges the training and the test sets to create one data set.
-
-dtSubjectTest <- fread("test\\subject_test.txt")
-dtSubjectTrain <- fread("train\\subject_train.txt")
-dtActivityTrain <- fread("train\\Y_train.txt")
-dtActivityTest <- fread("test\\Y_test.txt")
-#fread .... Error so i try an other way over ReadTable dataTable
-dtTrain <- data.table(read.table("train\\X_train.txt"))
-dtTest <- data.table(read.table("test\\X_test.txt"))
-
 dtSubject <- rbind(dtSubjectTrain, dtSubjectTest)
-setnames(dtSubject, "V1", "subject")
 dtActivity <- rbind(dtActivityTrain, dtActivityTest)
+
+#fread .... Error so i try an other way over ReadTable dataTable
+
+
+
+setnames(dtSubject, "V1", "subject")
+
 setnames(dtActivity, "V1", "activityNum")
 dt <- rbind(dtTrain, dtTest)
 
-> dtSubject <- cbind(dtSubject, dtActivity)
-> dt <- cbind(dtSubject, dt)
-> setkey(dt, subject, activityNum)
+dtSubject <- cbind(dtSubject, dtActivity)
+dt <- cbind(dtSubject, dt)
+setkey(dt, subject, activityNum)
 
 ##Extracts only the measurements on the mean and standard deviation for each measurement. 
-dtFeatures <- fread("features.txt")
-setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
 
-dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
-
-dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
-select <- c(key(dt), dtFeatures$featureCode)
-dt <- dt[, select, with = FALSE]
 
 
 
 
 
 ##Uses descriptive activity names to name the activities in the data set
-dtActivityNames <- fread("activity_labels.txt")
-setnames(dtActivityNames, names(dtActivityNames), c("activityNum", "activityName"))
+
 
 
 ##Appropriately labels the data set with descriptive variable names. 
-dt <- merge(dt, dtActivityNames, by = "activityNum", all.x = TRUE)
-setkey(dt, subject, activityNum, activityName)
-dt <- data.table(melt(dt, key(dt), variable.name = "featureCode"))
-dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by = "featureCode", 
-    all.x = TRUE)
-    
-dt$activity <- factor(dt$activityName)
-dt$feature <- factor(dt$featureName)
+
 
 
 ## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
