ab424bd7578c2c48efa0343ad528167a2717aaf9
Axji <axel.zenklusen@gmail.com>
2015-02-22 10:07:20 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -21,47 +21,59 @@ dt_X_Test <- read.table("test\\X_test.txt",header=FALSE)
 
 
 dt_Features <- read.table("features.txt",header=FALSE)
-dt_Activity_labels <- read.table("activity_labels.txt",header=FALSE);
-
 
 
 ## Name COlumns
-colnames(dtActivity_labels)  = c('activityId','activityType');
-colnames(dtTrain)        = dtFeatures[,2]; 
-colnames(yTrain)        = "activityId";
-colnames(subjectTrain)  = "subjectId";
+
+colnames(dt_X_Train)        = dt_Features[,2]; 
+colnames(dt_X_Test)        = dt_Features[,2]; 
+
+colnames(dt_Y_Train)        = "activityId";
+colnames(dt_Y_Test)        = "activityId";
+
+colnames(dt_Subject_Train)  = "subjectId";
+colnames(dt_Subject_Test)  = "subjectId";
+
+
 
 
 ##Merges the training and the test sets to create one data set.
-dtSubject <- rbind(dtSubjectTrain, dtSubjectTest)
-dtActivity <- rbind(dtActivityTrain, dtActivityTest)
+dt_Train = cbind(dt_X_Train, dt_Y_Train, dt_Subject_Train)
+dt_Test = cbind(dt_X_Test, dt_Y_Test, dt_Subject_Test)
 
-#fread .... Error so i try an other way over ReadTable dataTable
+dt_All <- rbind(dt_Train, dt_Test)
 
 
 
-setnames(dtSubject, "V1", "subject")
-
-setnames(dtActivity, "V1", "activityNum")
-dt <- rbind(dtTrain, dtTest)
-
-dtSubject <- cbind(dtSubject, dtActivity)
-dt <- cbind(dtSubject, dt)
-setkey(dt, subject, activityNum)
 
 ##Extracts only the measurements on the mean and standard deviation for each measurement. 
-
-
+dt_Features[,2] = gsub('-mean', 'Mean', dt_Features[,2])
+dt_Features[,2] = gsub('-std', 'Std', dt_Features[,2])
+dt_Features[,2] = gsub('[-()]', '', dt_Features[,2])
+columns <- grep(".*Mean.*|.*Std.*", dt_Features[,2])
+dt_Features <- dt_Features[columns,] 
+columns <- c(columns, 562, 563)
+dt_All <- dt_All[,columns] 
 
 
 
 
 ##Uses descriptive activity names to name the activities in the data set
+dt_Activity_labels <- read.table("activity_labels.txt",header=FALSE);
 
-
-
-##Appropriately labels the data set with descriptive variable names. 
-
+#I tried with a For Loop and dt_Activity_Labels ... but had no success (there are only 6 Values => )
+dt_All$activityId <- gsub(1, "Walking", dt_All$activityId)
+dt_All$activityId <- gsub(2, "WALKING_UPSTAIRS", dt_All$activityId)
+dt_All$activityId <- gsub(3, "WALKING_DOWNSTAIRS", dt_All$activityId)
+dt_All$activityId <- gsub(4, "SITTING", dt_All$activityId)
+dt_All$activityId <- gsub(5, "STANDING", dt_All$activityId)
+dt_All$activityId <- gsub(6, "LAYING", dt_All$activityId)
 
 
 ## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+IDs   = c("subjectId", "activityId")
+data_labels = setdiff(colnames(dt_All), IDs)
+melted_data = melt(dt_All, id = IDs, measure.vars = data_labels)
+tidy = dcast(melted_data, subjectId + activityId ~ variable, mean)
+
+write.table(tidy, file = "tidy_data.txt")
